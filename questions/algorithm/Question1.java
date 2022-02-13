bd197cdb74be8115381bd4adfdc834856ff86646
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-02-13 15:53:43 +0900

    feat: file create

---
+++ questions/algorithm/Question1.java
@@ -0,0 +1,10 @@
+/**
+ * Javaの演習問題：アルゴリズム編
+ * 配列
+ * https://eng-entrance.com/java-question-algorithm#i-2
+ */
+package questions.algorithm;
+
+public class Question1 {
+  
+}
---
+++ questions/knowledge/Question1.java
@@ -0,0 +1,10 @@
+/**
+ * Javaの練習問題：知識編
+ * java.ioパッケージ　テキストファイル入出力
+ * https://eng-entrance.com/java-question-knowledge#javaio
+ */
+package questions.knowledge;
+
+public class Question1 {
+  
+}
